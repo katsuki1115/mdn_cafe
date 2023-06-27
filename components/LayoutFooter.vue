34901b0718515fa028c780c1c4c93d8e893327ab
DESKTOP-JM33RC1\no1s <katsuki.rintaro@no1s.biz>
2023-06-27 15:39:16 +0900

    chapter2 finish

---
+++ components/LayoutFooter.vue
@@ -0,0 +1,14 @@
+<template>
+	<!--フッター-->
+	<footer class="bg-black pb-10 pt-10">
+		<p class="text-center text-white text-sm font-bold leading-relaxed whitespace-no-wrap uppercase">
+			Copyright © 2020 NUXT SMAPLE SITE DEMO
+		</p>
+	</footer>
+</template>
+
+<script>
+	export default {
+		name: 'LayoutFooter',
+	}
+</script>
